6074ff9b2214fc49d980a16304c8195f1b7b58b2
katydid0519-ctrl <katydid0519@gmail.com>
2026-04-03 08:40:32 +0900

    Add files via upload

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -32,7 +32,8 @@ exports.handler = async function (event) {
       "www.moel.go.kr",
       "moel.go.kr",
       "kosha.or.kr",
-      "www.kosha.or.kr"
+      "www.kosha.or.kr",
+      "query2.finance.yahoo.com"
     ]);
 
     if (!allowedHosts.has(url.hostname)) {
